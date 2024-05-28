46f4572747263d681bdfb66e44e9672f992cd65b
Erik Leclercq <rkleclercq@gmail.com>
2024-05-28 15:07:52 +0200

    Average grid search moment

--- half_cheetah.py
+++ half_cheetah.py
@@ -18,14 +18,14 @@ def fitness_func(ga_instance, solution, sol_idx):
     sum_reward = 0
     truncated = False
     num_steps = 0
-    
+
     while not truncated:
         observation_tensor = torch.tensor(observation, dtype=torch.float)
         q_val = model(observation_tensor).detach().numpy()
         q_val = np.clip(q_val, action_min, action_max)
         observation, reward, done, truncated, info = env.step(q_val)
         sum_reward += reward
-        num_steps +=1
+        num_steps += 1
 
     return sum_reward
 
@@ -34,22 +34,20 @@ def on_generation(ga_instance):
     generation = ga_instance.generations_completed
 
     # Print generation and fitness of the best individual
-    gen = ga_instance.generations_completed
+    gen = generation
     fit = ga_instance.best_solution()[1]
-    print(
-        f"\rGeneration = {gen}, Fitness = {fit}",
-        end="", flush=True
-    )
+    print(f"\rGeneration = {gen}, Fitness = {fit}", end="", flush=True)
 
     # Record one run of the agent at the beginning and middle of the training
     if generation == 1 or generation == 25:
-        tmp_env = gym.make("HalfCheetah-v4", render_mode='rgb_array')
-        tmp_env = RecordVideo(env=tmp_env, video_folder="test_folder",
-                              name_prefix=f"{generation}-training")
+        tmp_env = gym.make("HalfCheetah-v4", render_mode="rgb_array")
+        tmp_env = RecordVideo(
+            env=tmp_env, video_folder=filename, name_prefix=f"{generation}-training"
+        )
 
         observation, _ = tmp_env.reset()
         truncated = False
-        
+
         while not truncated:
             observation_tensor = torch.tensor(observation, dtype=torch.float)
             q_val = model(observation_tensor).detach().numpy()
@@ -58,30 +56,32 @@ def on_generation(ga_instance):
         tmp_env.close()
 
 
-
 def myModel(observation_space, units, action_space):
     model = torch.nn.Sequential()
 
     model = torch.nn.Sequential(
-        torch.nn.Linear(in_features=observation_space, out_features=units, ),
+        torch.nn.Linear(
+            in_features=observation_space,
+            out_features=units,
+        ),
         torch.nn.Tanh(),
         torch.nn.Linear(units, action_space),
-        torch.nn.Identity()
+        torch.nn.Identity(),
     )
     return model
 
-def create_GA_instance(torch_ga, num_solutions) -> pygad.GA:
+
+def create_GA_instance(torch_ga, num_solutions, pst, ct, mt) -> pygad.GA:
     NUM_GEN = 50
     NUM_PARENTS_MATING = num_solutions // 2
     INIT_POP = torch_ga.population_weights
-    PARENT_SELECTION_TYPES = "sss", "rws", 'rank', 'tournament'
-    PARENT_SELECTION_TYPE = "tournament"
-    CROSSOVER_TYPES = "two_points", 'single_point', 'uniform', 'scattered'
-    CROSSOVER_TYPE = "uniform"
-    MUTATION_TYPES = "random", "swap", "inversion", "scramble", "adaptive"
-    MUTATION_TYPE = "adaptive"
-    MUTATION_PERCENTS_GENES = 10, 30, 60, 90
-    MUTATION_PERCENT_GENES = (60,20)
+    # PARENT_SELECTION_TYPES = "sss", "rws", "rank", "tournament"
+    PARENT_SELECTION_TYPE = pst
+    # CROSSOVER_TYPES = "two_points", "single_point", "uniform", "scattered"
+    CROSSOVER_TYPE = ct
+    # MUTATION_TYPES = "random", "swap", "inversion", "scramble", "adaptive"
+    MUTATION_TYPE = mt
+    MUTATION_PERCENT_GENES = 30
     KEEP_PARENTS = 0
     KEEP_ELITISM = 2
 
@@ -97,15 +97,28 @@ def create_GA_instance(torch_ga, num_solutions) -> pygad.GA:
         keep_parents=KEEP_PARENTS,
         keep_elitism=KEEP_ELITISM,
         on_generation=on_generation,
-        parallel_processing=["process", 10]
+        parallel_processing=["process", 15],
     )
-    ga_instance.summary()
-    params = PARENT_SELECTION_TYPE + '_' + CROSSOVER_TYPE + '_' + MUTATION_TYPE + '_' + str(num_solutions) + '_' + str(MUTATION_PERCENT_GENES)
+
+    translation_table = str.maketrans({" ": None, "(": None, ")": None, ",": "_"})
+
+    # ga_instance.summary()
+    params = (
+        PARENT_SELECTION_TYPE
+        + "_"
+        + CROSSOVER_TYPE
+        + "_"
+        + MUTATION_TYPE
+        + "_"
+        + str(num_solutions)
+        + "_"
+        + str(MUTATION_PERCENT_GENES).translate(translation_table)
+    )
     return params, ga_instance
 
 
 def main() -> None:
-    global env, model, observation_size, action_min, action_max
+    global env, model, observation_size, action_min, action_max, filename
 
     env = gym.make("HalfCheetah-v4")
 
@@ -125,68 +138,77 @@ def main() -> None:
     num_solutions = 30
 
     solutions, solution_fits, fit_evo = [], [], []
+    PARENT_SELECTION_TYPES = ["sss", "rws", "rank", "tournament"]
+    CROSSOVER_TYPES = ["two_points", "single_point", "scattered"]
+    MUTATION_TYPES = ["swap"]
 
-    for _ in range(5):
-        model = myModel(
-            observation_space=observation_size, units=24, action_space=action_space
-        )
-        torch_ga = pygad.torchga.TorchGA(model=model, num_solutions=num_solutions)
-        filename, ga_instance = create_GA_instance(torch_ga, num_solutions)
-        ga_instance.run()
-        env.close()
+    for parent in PARENT_SELECTION_TYPES:
+        for crossover in CROSSOVER_TYPES:
+            for mutation in MUTATION_TYPES:
+                for _ in range(5):
+                    model = myModel(
+                        observation_space=observation_size, units=24, action_space=action_space
+                    )
+                    torch_ga = pygad.torchga.TorchGA(model=model, num_solutions=num_solutions)
+                    filename, ga_instance = create_GA_instance(torch_ga, num_solutions, parent, crossover, mutation)
+                    ga_instance.run()
+                    env.close()
 
-        solution, solution_fit, _ = ga_instance.best_solution()
+                    solution, solution_fit, _ = ga_instance.best_solution()
 
-        solutions.append(solution)
-        solution_fits.append(solution_fit)
-        fit_evo.append(ga_instance.best_solutions_fitness)
+                    solutions.append(solution)
+                    solution_fits.append(solution_fit)
+                    fit_evo.append(ga_instance.best_solutions_fitness)
+
+                # Taking best solution out of 5 runs
+                solution_fits = np.array(solution_fits)
+                solution = solutions[np.argmax(solution_fits)]
+                mean_fit = solution_fits.mean()
+
+                plt.figure(figsize=(10, 6))
+                for i, fitness_history in enumerate(fit_evo):
+                    plt.plot(fitness_history, label=f"Run {i+1}")
+                plt.xlabel("Generation")
+                plt.ylabel("Fitness")
+                plt.title(f"{filename} with mean fitness: {mean_fit}")
+                plt.legend()
+                plt.savefig(filename)
+                plt.show()
+
+                #     ga_instance.run()
+                #     env.close()
+                #
+                #     solution, solution_fitness, solution_idx = ga_instance.best_solution()
+                #     print(f"Fitness value of the best solution = {solution_fitness}")
+                #     print(f"Index of the best solution : {solution_idx}")
 
 
-    plt.figure(figsize=(10, 6))
-    for i, fitness_history in enumerate(fit_evo):
-        plt.plot(fitness_history, label=f'Run {i+1}')
-    plt.xlabel('Generation')
-    plt.ylabel('Fitness')
-    plt.title('Fitness Evolution over Generations')
-    plt.legend()
-    plt.savefig(filename)
-    plt.show()
+                # Load the best solution into the model
+                model_weights_dict = pygad.torchga.model_weights_as_dict(
+                    model=model, weights_vector=solution
+                )
+                model.load_state_dict(model_weights_dict)
 
-#     ga_instance.run()
-#     env.close()
-# 
-#     solution, solution_fitness, solution_idx = ga_instance.best_solution()
-#     print(f"Fitness value of the best solution = {solution_fitness}")
-#     print(f"Index of the best solution : {solution_idx}")
+                tmp_env = gym.make("HalfCheetah-v4", render_mode="rgb_array")
+                env = gym.wrappers.RecordVideo(env=tmp_env, video_folder=filename)
 
-    # Taking best solution out of 5 runs
-    best_id = solution_fits.index(max(solution_fits))
-    solution = solutions[best_id]
+                # play game and record the final run of the cheetah
+                observation, _ = env.reset()
+                sum_reward = 0
 
+                trunctated = False
+                while not trunctated:
 
-    #Load the best solution into the model
-    model_weights_dict = pygad.torchga.model_weights_as_dict(model=model, weights_vector=solution)
-    model.load_state_dict(model_weights_dict)
-    
-    tmp_env = gym.make("HalfCheetah-v4", render_mode="rgb_array")
-    env = gym.wrappers.RecordVideo(env=tmp_env, video_folder="test_folder")
+                    observation_tensor = torch.tensor(observation, dtype=torch.float)
+                    q_val = model(observation_tensor).detach().numpy()
+                    q_val = np.clip(q_val, action_min, action_max)
+                    observation, reward, done, trunctated, _ = env.step(q_val)
+                    sum_reward += reward
 
-    # play game
-    observation, _ = env.reset()
-    sum_reward = 0
+                print(f"Agent achieved {sum_reward} on the final run")
+                env.close()
+                # ga_instance.save(filename=filename) # save the ga_instance to a pkl file
 
-    trunctated = False
-    while not trunctated:
-
-        observation_tensor = torch.tensor(observation, dtype=torch.float)
-        q_val = model(observation_tensor).detach().numpy()
-        q_val = np.clip(q_val, action_min, action_max)
-        observation, reward, done, trunctated, _ = env.step(q_val)
-        sum_reward += reward
-
-    print(f"Agent achieved {sum_reward} on the final run")
-    env.close()
-    ga_instance.save(filename=filename)
 
 if __name__ == "__main__":
     main()
